406a9a0c5593dd9401f2ec349fb1e8587cd77120
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-09-23 23:16:46 -0400

    Create 004_are_quadrado.py

---
+++ 004_are_quadrado.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#Faca um Programa que calcule a area de um quadrado, em seguida mostre o dobro desta area para o usuario.
+
+area = float(input("Insert area do quadrado: "))
+print("Area do quadrado inserida %r") % (area)
+mult = (area*area)
+area_total = (mult)
+print("area do quadrado: %r") % (area_total)
+dobro = (area_total + area_total)
+print("Dobro do area : %r") % (dobro)

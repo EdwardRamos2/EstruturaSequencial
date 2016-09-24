8b8125acbf0da9da4b74a4af2c9ef1a549051782
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-09-23 21:47:39 -0400

    Create metros_in_centimetros.py

---
+++ metros_in_centimetros.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#Faaa um Programa que converta metros para centimetros.
+
+
+metros = float(input("Insert value in mtros"))
+centimetros = (metros)
+centimetros *= 100
+print("valor em metros inserido: %d M e o valor em centimetros: %d Cm\n") % (metros,centimetros)
+
+

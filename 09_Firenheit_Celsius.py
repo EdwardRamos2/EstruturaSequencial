26d727771d5ea047641890f52b07f9f4733add80
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-09-24 20:24:51 -0400

    Create 09_Firenheit_Celsius.py

---
+++ 09_Firenheit_Celsius.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# Faca um Programa que peca a temperatura em graus Farenheit, transforme e mostre a temperatura em graus Celsius.
+
+#    C = (5 * (F-32) / 9).
+
+f = float(input("Value Farenheit:"))
+
+convert = (5 * (f - 32) / 9)
+print(convert)

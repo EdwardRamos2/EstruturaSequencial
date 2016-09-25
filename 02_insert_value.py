1a6db0b3c301631bb51dc94e1e0ced348226b58b
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-09-24 20:34:52 -0400

    Create 02_insert_value.py

---
+++ 02_insert_value.py
@@ -0,0 +1,5 @@
+#/usr/bin/env python
+#Facca um Programa que peca um numero e entao mostre a mensagem O numero informado foi numero.
+
+value_one = int(input("Insert one number integer"))
+print(value_one)

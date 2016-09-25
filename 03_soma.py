2b8bbfc66bc29ac9efe66c81d5bae58a2dee7089
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-09-24 20:34:04 -0400

    Create 03_soma.py

---
+++ 03_soma.py
@@ -0,0 +1,10 @@
+#/usr/bin/env python
+
+#FaCa um Programa que p5eCa dois nUmeros e imprima a soma.
+
+value1 = int(input("insert number:\n"))
+value2 = int(input("insert outher number:\n'"))
+
+value3 = (value1 + value2)
+print("A soma do valor %d + %d\n") % (value1,value2)
+print(value3)

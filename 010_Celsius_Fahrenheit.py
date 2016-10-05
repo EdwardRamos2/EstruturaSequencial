1dc676c39d21b5a0b7c9725eb57c909a3988434f
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-10-04 22:28:46 -0400

    Update 010_Celsius_Fahrenheit.py

--- 010_Celsius_Fahrenheit.py
+++ 010_Celsius_Fahrenheit.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+#*-*coding: utf-8 *-*
+#Autor: EdwardRamos
+
+# Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Farenheit. 
 
 c = float(input("Insert value in Celsius:"))
 

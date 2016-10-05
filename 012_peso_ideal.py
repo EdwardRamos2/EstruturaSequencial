19ce36f99518c324245140c89376de167191da8d
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-10-04 22:29:47 -0400

    Update 012_peso_ideal.py

--- 012_peso_ideal.py
+++ 012_peso_ideal.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+#*-*coding: utf-8 *-*
+#Autor: EdwardRamos
+
+# Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal,
+#usando a seguinte fórmula: (72.7*altura) - 58 
+
 print("(+)     Your ideal weight!!!!!")
 height = float(raw_input("Enter your height: "))
 if height != 0:
